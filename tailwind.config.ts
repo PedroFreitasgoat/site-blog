93e92cd942508a05044972263865dae63c6e23af
pedro <pedrogithb1@gmail.com>
2026-05-30 17:50:47 -0300

    Entendendo o Roteamento do Next.js Header Lasyout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,67 @@
 import type { Config } from "tailwindcss";
 
-export default  {
-  content: [
-    './src/pages/**/*.{js,jsx,tsx,mdx}',
-    './src/components/**/*.{js,jsx,tsx,mdx}',
-    './src/app/**/*.{js,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config
+export default {
+  darkMode: ["class"],
 
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        border: "oklch(var(--border))",
+        input: "oklch(var(--input))",
+        ring: "oklch(var(--ring))",
+
+        background: "oklch(var(--background))",
+        foreground: "oklch(var(--foreground))",
+
+        primary: {
+          DEFAULT: "oklch(var(--primary))",
+          foreground: "oklch(var(--primary-foreground))",
+        },
+
+        secondary: {
+          DEFAULT: "oklch(var(--secondary))",
+          foreground: "oklch(var(--secondary-foreground))",
+        },
+
+        destructive: {
+          DEFAULT: "oklch(var(--destructive))",
+          foreground: "oklch(var(--primary-foreground))",
+        },
+
+        muted: {
+          DEFAULT: "oklch(var(--muted))",
+          foreground: "oklch(var(--muted-foreground))",
+        },
+
+        accent: {
+          DEFAULT: "oklch(var(--accent))",
+          foreground: "oklch(var(--accent-foreground))",
+        },
+
+        popover: {
+          DEFAULT: "oklch(var(--popover))",
+          foreground: "oklch(var(--popover-foreground))",
+        },
+
+        card: {
+          DEFAULT: "oklch(var(--card))",
+          foreground: "oklch(var(--card-foreground))",
+        },
+      },
+
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+
+  plugins: [],
+} satisfies Config;
