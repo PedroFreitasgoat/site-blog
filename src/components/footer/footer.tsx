fc04d40500bae9ffa2d455b59d208589ee447156
pedro <pedrogithb1@gmail.com>
2026-06-04 20:19:53 -0300

    Adicionando Style Guide

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export const Footer = () => {
     return (
-        <footer className="border-t border-white/10">
+        <footer className="bg-gray-500">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
              <div className="flex justify-between md:flex-grow gap-8 py-8">
 
@@ -16,11 +16,11 @@ export const Footer = () => {
                 />
                 </Link>
 
-                <nav className="flex flex-col text-white md:flex-row items-center gap-4
-                text-sm text-muted-foreground">
-                    <Link href='/termos-de-uso' className="hover:text-primary">Termos de uso</Link>
-                    <Link href='/politica-de-privacidade' className="hover:text-primary">Politica de privacidade</Link>
-                    <Link href='/feedback' className="hover:text-primary">Feedback</Link>
+                <nav className="flex flex-col md:flex-row items-center gap-4
+                text-sm text-blue-100">
+                    <Link href='/termos-de-uso' className="hover:text-blue-200">Termos de uso</Link>
+                    <Link href='/politica-de-privacidade' className="hover:text-blue-200">Politica de privacidade</Link>
+                    <Link href='/feedback' className="hover:text-blue-200">Feedback</Link>
 
                 </nav>
              </div>
