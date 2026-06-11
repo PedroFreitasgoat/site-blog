2ee35ac758526985f840f4d80a65a0282e53b7a0
pedro <pedrogithb1@gmail.com>
2026-06-11 20:40:26 -0300

    Seção: Support

--- src/components/suport-section/support-section.tsx
+++ src/components/suport-section/support-section.tsx
@@ -1,7 +1,12 @@
 export const SupportSection = () => {
  return (
-    <div>
-        
-    </div>
+    <section>
+        <h2>Sua loja de afiliados, simples, do jeito que deveria ser</h2>
+
+        <div>
+         <strong>Personalize o seu site</strong>
+         <p>Adicione sua logo, favicon, cores no seu catalogo e tenha tudo com a sua cara.</p>
+        </div>
+    </section>
  )   
 }
