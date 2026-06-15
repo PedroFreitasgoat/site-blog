376c8159af276b92aa9dd7ddeb21c9ca513a049e
pedro <pedrogithb1@gmail.com>
2026-06-15 12:57:51 -0300

    support

--- src/components/suport-section/support-section.tsx
+++ src/components/suport-section/support-section.tsx
@@ -3,7 +3,7 @@ export const SupportSection = () => {
     <section>
         <h2>Sua loja de afiliados, simples, do jeito que deveria ser</h2>
 
-        <div>
+        <div className="flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-blue-400">
          <strong>Personalize o seu site</strong>
          <p>Adicione sua logo, favicon, cores no seu catalogo e tenha tudo com a sua cara.</p>
         </div>
